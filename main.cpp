80fbafd11dc9974babac8d38b9bb1f2dbf1847a8
Deins <deinsegle@gmail.com>
2014-10-04 09:23:41 +0300

    drawCircleOutline interface change
    swaped drawCircleOutline arguments 'w' and 'segments'

--- main.cpp
+++ main.cpp
@@ -5,19 +5,28 @@ using namespace kjpsgl;
 
 int main()
 {
-    init(1024,720,false,0);
-    setVsync(true);
-
-    int tex = loadTexture("data/kaspars.png");
+    init(1024,720);
 
     // galvenais cikls
     while (!getKey("Escape")) // kamēr nav nospiesta escape poga
     {
         update();
-        setTexture(tex);
+
+        clearScreen(0,0,0);
+        setColor(44,44,44);
+        drawRectangle(512-111,350-250,512+111,350+250);
         setColor(255,0,0);
-        drawLine(0,0,666,0,64);
-        drawRectangle(0,0,64,64);
+        drawCircle(512,350+155,64);
+        setColor(255,255,0);
+        drawCircle(512,350,64);
+        setColor(0,255,0);
+        drawCircle(512,350-155,64);
+
+        setColor(0,0,0,122);
+        for (int i=0; i<3; ++i){
+            if ((getTime()/1000)%3!=i)
+                drawCircle(512,350-155+155*i,64);
+        }
         display();
     }
 }
